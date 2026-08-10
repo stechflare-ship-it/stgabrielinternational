dbed73dc2720ef8efd69ccd08bef697efaf82446
Techflare Solutions <stechflare@gmail.com>
2026-08-10 18:45:11 +0300

    Fix: import BrowserRouter from react-router-dom (routing error)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const container = document.getElementById('root');
