6f6d84a204a4aca5ea9f41cbfd9b785f819102b9
Techflare Solutions <stechflare@gmail.com>
2026-08-19 17:45:09 +0300

    Fix import statement formatting for boardingbackground

--- src/pages/BoardingPage.tsx
+++ src/pages/BoardingPage.tsx
@@ -5,7 +5,7 @@ import { SectionHeading } from '../components/SectionHeading';
 import { BoardingSection } from '../components/BoardingSection';
 import { FeeTable } from '../components/FeeTable';
 import { RequirementsChecklist } from '../components/RequirementsChecklist';
-import boardingbackground from '../assets/images/boardinglife/boardingbackground.webp ';
+import boardingbackground from '../assets/images/boardinglife/boardingbackground.webp';
 import boardingfacility from '../assets/images/boardinglife/boardingfacility.webp';
 
 export const BoardingPage: React.FC = () => {
